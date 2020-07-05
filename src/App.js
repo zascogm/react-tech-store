7941835c1db645d2f747331cdb47d2ad84f9b56e
zascogm <zascogm@gmail.com>
2020-07-05 11:14:34 +1000

    Adding react-route, basic functionality for App.js, extra images, folder structure

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import About from './pages/AboutPage'
+import Cart from './pages/CartPage'
+import Contact from './pages/ContactPage'
+import Default from './pages/Default'
+import Home from './pages/HomePage'
+import Products from './pages/ProductsPage'
+import SingleProduct from './pages/SingleProductPage'
+
+import {Route, Switch} from 'react-router-dom'
+
 function App() {
   return (
-    <h1>Hello from tech store</h1>
+    <>
+      {/* navbar, sidebar, cart, footer */}
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/cart" component={Cart}/>
+        <Route path="/contact" component={Contact}/>
+        <Route path="/products" exact component={Products}/>
+        <Route path="/products/:id" component={SingleProduct}/>
+        <Route component={Default}/>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>Hello from Home page</h1>
+        </>
+    )
+}
